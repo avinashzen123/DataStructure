7a4e4dfa640101a61018a1a7f7e3d3b40b3e1c6e
avinashzen123 <avinashzen123@gmail.com>
2023-03-08 09:06:18 +0530

    Added comment on hamiltonian cycle

--- src/com/avinash/backtracking/HamiltonianCycleBackTracking.java
+++ src/com/avinash/backtracking/HamiltonianCycleBackTracking.java
@@ -1,83 +1,88 @@
 package com.avinash.backtracking;
 
+import java.util.Arrays;
+
 public class HamiltonianCycleBackTracking {
 
-	private int numOfVertexes;
-	private int[] hamiltonianPath;
-	private int[][] adjacencyMatrix;
-	
-	public HamiltonianCycleBackTracking(int[][] adjacencyMatrix) {
-		this.adjacencyMatrix = adjacencyMatrix;
-		this.numOfVertexes = adjacencyMatrix[0].length;
-		this.hamiltonianPath = new int[numOfVertexes];
-	}
-	
-	public void solve() {
-		hamiltonianPath[0] = 0;
-		if (findSolution(1)) {
-			showResult();
-		} else {
-			System.out.println("There is no solution");
-		}
-	}
-	
-	private boolean findSolution (int position) {
-		// If we have considered all the vertices so end of the algorithm
-		if (position == this.numOfVertexes) {
-			if (adjacencyMatrix[hamiltonianPath[position - 1]][hamiltonianPath[0]] == 1) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-		
-		//Try all the possible vertexes in the graphs
-		for (int vertexIndex = 1; vertexIndex < numOfVertexes; vertexIndex++) {
-			if (isValid(vertexIndex, position)) {
-				// Include in the vertex in hamiltonian path
-				hamiltonianPath[position] = vertexIndex;
-				if (findSolution(position + 1)) {
-					return true;
-				} 
-				// Backtrack
-				// Here backtracking does nothing, we just consider another vertex
-			}
-		}
-		return false;
-	}
-	
-	private boolean isValid(int vertex, int actualPosition) {
-		// First criteria, whether the two nodes are connected or not.
-		if (adjacencyMatrix[hamiltonianPath[actualPosition - 1]][vertex] == 0) {
-			return false;
-		}
-		for (int i = 0; i < actualPosition; i ++) {
-			if (hamiltonianPath[i] == vertex) {
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	private void showResult() {
-		System.out.println("Hamiltonian cycle exists");
-		for (int i = 0; i < hamiltonianPath.length; i++) {
-			System.out.print(hamiltonianPath[i] + " - ");
-		}
-		System.out.println(hamiltonianPath[0]);
-	}
-	
+	/*
+     * Utility function to check if the vertex v can be added to at index 'pos' int
+     * the hamiltonian cycle
+     * constructed so far stored in path[]
+     */
+    private boolean isSafe(int vertex, int graph[][], int path[], int pos) {
+        /*
+         * check if this vertex is and adjacent vertex of the previously
+         * added vertex
+         */
+        if (graph[path[pos - 1]][vertex] == 0) {
+            return false;
+        }
+        /*
+         * Check if the vertex has already been included.
+         * This step can be optimized by creating an array
+         * of size V
+         */
+        for (int i = 0; i < pos; i++) {
+            if (path[i] == vertex) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean hamCycleUtil(int graph[][], int path[], int pos) {
+        if (pos == graph.length) {
+            if (graph[path[pos - 1]][path[0]] == 1) {
+                return true;
+            }
+            return false;
+        }
+        /*
+         * Try different vertices as a next candidate in Hamiltonian
+         * cycle. We don't try for 0 as we included 0 as starting point
+         * in hamCycle()
+         */
+        for (int vertex = 1; vertex < graph.length; vertex++) {
+            // Check if this vertex can be added to Hamiltonian cycle
+            if (isSafe(vertex, graph, path, pos)) {
+                path[pos] = vertex;
+                if (hamCycleUtil(graph, path, pos + 1)) {
+                    return true;
+                }
+                // IF adding vertex v does not lead to a solution the remove it
+                path[pos] = -1;
+            }
+        }
+        /*
+         * If not vertex can be added to hamiltonian cycle constructed
+         * so far the return false
+         */
+        return false;
+    }
+
+    public void hamCycle(int graph[][]) {
+        int path[] = new int[graph.length];
+        Arrays.fill(path, -1);
+        path[0] = 0;
+        if (hamCycleUtil(graph, path, 1)) {
+            showResult(path);
+        } else {
+            System.out.println("No solution exist");
+        }
+    }
+
+    private void showResult(int[] hamiltonianPath) {
+        System.out.println("Hamiltonian cycle exists");
+        for (int i = 0; i < hamiltonianPath.length; i++) {
+            System.out.print(hamiltonianPath[i] + " - ");
+        }
+        System.out.println(hamiltonianPath[0]);
+    }
 	public static void main(String[] args) {
-		int[][] adjacencyMatrix = {
-				{0,1,0,0,0,1},
-				{1,0,1,0,0,0},
-				{0,1,0,0,1,0},
-				{0,0,0,0,1,1},
-				{0,0,1,1,0,1},
-				{1,0,0,1,1,0}
-		};
-		
-		HamiltonianCycleBackTracking backTracking  = new HamiltonianCycleBackTracking(adjacencyMatrix);
-		backTracking.solve();
+		int[][] adjacencyMatrix = { { 0, 1, 0, 0, 0, 1 }, { 1, 0, 1, 0, 0, 0 }, { 0, 1, 0, 0, 1, 0 },
+				{ 0, 0, 0, 0, 1, 1 }, { 0, 0, 1, 1, 0, 1 }, { 1, 0, 0, 1, 1, 0 } };
+
+		HamiltonianCycleBackTracking backTracking = new HamiltonianCycleBackTracking();
+		backTracking.hamCycle(adjacencyMatrix);
 	}
 }
