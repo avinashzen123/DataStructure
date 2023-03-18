9e8bd658b4bdaa3ce0e35f7aec73e61da8c0fa96
Avinash Kumar Sharma <avinashzen123@gmail.com>
2023-03-18 15:34:32 +0530

    Updated hit counter

--- src/com/avinash/interview/HitCounter.java
+++ src/com/avinash/interview/HitCounter.java
@@ -1,5 +1,31 @@
 package com.avinash.interview;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class HitCounter {
+	
+	static class UsingQueue {
+		private Queue<Integer> hits;
+		
+		public UsingQueue() {
+			this.hits = new LinkedList<>();
+		}
+		
+		// Time complexity : O(1)
+		public void hit(int timeStamp) {
+			this.hits.add(timeStamp);
+		}
+		// Time complexity : O(n)ß
+		public int getHits(int timeStamp) {
+			while(!this.hits.isEmpty()) {
+				int diff = timeStamp - this.hits.peek();
+				if (diff >= 300) this.hits.remove();
+				else break;
+			}
+		
+			return this.hits.size();
+		}
+	}
 
 }
